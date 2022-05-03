a1b3dcf448a6e2e7153725c91a337f51f807e83d
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-05-03 23:52:12 +0900

    📚220426 K번째 큰 수

---
+++ 카카오 코테/[1차]비밀지도.js
@@ -0,0 +1,21 @@
+function solution(n, arr1, arr2) {
+  const binArr1 = [];
+  const binArr2 = [];
+  let ansArr = [];
+
+  arr1.forEach((v) => binArr1.push(v.toString(2).padStart(n, 0).split("")));
+  arr2.forEach((v) => binArr2.push(v.toString(2).padStart(n, 0).split("")));
+  console.log(binArr1, binArr2);
+
+  for (let i = 0; i < n; i++) {
+    let temp = [];
+    for (let j = 0; j < n; j++) {
+      let num = Number(binArr1[i][j]) + Number(binArr2[i][j]);
+      let str = num === 0 ? " " : "#";
+      temp.push(str);
+    }
+    ansArr.push(temp);
+  }
+  console.log(ansArr);
+  return ansArr.map((ans) => ans.join(""));
+}
